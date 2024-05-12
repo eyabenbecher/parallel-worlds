69b132d02a68d8950aeeae62cf078bd5bbdba3b2
eyabenbecher <150703203+eyabenbecher@users.noreply.github.com>
2024-05-12 14:11:57 +0100

    pic added

--- parallelworlds/Assets/myscripts/Decode.cs
+++ parallelworlds/Assets/myscripts/Decode.cs
@@ -7,10 +7,12 @@ public class Decode : MonoBehaviour
 {
     [SerializeField] private TMP_InputField inputField;
     [SerializeField] private TMP_Text decodedText;
+    [SerializeField] private GameObject picture;
     private string code = "itperuc";
 
     private void Start()
     {
+        picture.gameObject.SetActive(false);
         decodedText.gameObject.SetActive(false);
         inputField.onValueChanged.AddListener(delegate { CheckText(); });
     }
@@ -23,6 +25,8 @@ public class Decode : MonoBehaviour
         {
            
             decodedText.gameObject.SetActive(true);
+            picture.gameObject.SetActive(true);
+
         }
         else
         {
